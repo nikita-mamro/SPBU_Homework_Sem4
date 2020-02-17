2b8a14383821c6d680ef153353550c518f49a318
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2020-02-17 15:40:48 +0300

    Calculating factorial in F#

---
+++ Lab1/Facrtorial.Tests/FactorialTests.fs
@@ -0,0 +1,17 @@
+module Facrtorial.Tests
+
+open NUnit.Framework
+open Factorial
+
+[<TestCase (0, 1)>]
+[<TestCase (1, 1)>]
+[<TestCase (2, 2)>]
+[<TestCase (5, 120)>]
+[<TestCase (10, 3628800)>]
+let FactorialTest (input, expected) =
+    Assert.AreEqual(Some(expected), factorial input)
+
+[<TestCase (-1)>]
+[<TestCase (-1000000)>]
+let FactorialNoneTest (input) = 
+    Assert.AreEqual(None, factorial input)
---
+++ Lab1/Factorial/Factorial.fs
@@ -0,0 +1,12 @@
+﻿module  Factorial
+
+let factorial n =
+    let rec recursiveFactorial n acc =
+        if n < 0 then
+            None
+        else if n = 0 || n = 1 then
+            Some(acc)
+        else
+            recursiveFactorial (n - 1) (acc * n)
+
+    recursiveFactorial n 1
