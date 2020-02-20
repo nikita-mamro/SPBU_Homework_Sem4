854873e87b90845b23884982549c04bc99f3ee00
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2020-02-20 15:05:30 +0300

    Finding position of element in the list

---
+++ Lab1/Task5/FirstEntry/FirstEntry.fs
@@ -0,0 +1,12 @@
+﻿module FirstEntry
+
+let firstEntry x list =
+    let rec firstEntryRecursive x list acc = 
+        match list with
+        | head :: tail ->
+                if head = x then
+                    Some(acc)
+                else 
+                    firstEntryRecursive x tail (acc + 1)
+        | [] -> None
+    firstEntryRecursive x list 0
