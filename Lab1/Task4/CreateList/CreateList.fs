67fd2ccf86d9dd8b148325adae66e1f2484d61fb
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2020-02-20 14:54:05 +0300

    Creating list of powers of 2 in F#

---
+++ Lab1/Task4/CreateList/CreateList.fs
@@ -0,0 +1,20 @@
+﻿module CreateList
+
+let createList n m =
+    let rec power n m =
+        match m with
+        | 0 -> 1
+        | 1 -> n
+        | 2 -> n * n
+        | _ -> (power n (m / 2)) * (power n (m / 2)) * (power n (m % 2))
+    
+    let rec createRecursive list n m =
+        if m = -1 then
+            list
+        else
+            createRecursive (power 2 (n + m) :: list) n (m - 1)
+
+    if n < 0 || m < 0 then
+        None
+    else
+        Some(createRecursive [] n m)
