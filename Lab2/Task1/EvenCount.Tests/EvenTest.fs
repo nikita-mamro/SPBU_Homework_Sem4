4c7527a0120b58d8ef23b6069efcc4193fc01f6c
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2020-02-24 13:16:25 +0300

    Count even numbers in a list

---
+++ Lab2/Task1/EvenCount.Tests/EvenTest.fs
@@ -0,0 +1,32 @@
+module EvenCount.Tests
+
+open NUnit.Framework
+open EvenCount
+
+let testData () = 
+    [
+        [], 0
+        [0], 1
+        [1], 0
+        [-1], 0
+        [-2], 1
+        [0; 2; 4; 6; 8; 10], 6 
+        [1; 3; 5; 7; 9; 11], 0 
+        [0; 1; 3], 1
+        [1; 0; 3], 1
+        [1; 3; 0], 1
+        [1; 2; 3], 1
+        [0; 1; 2; 3; 4], 3
+    ] |> List.map (fun (list, expected) -> TestCaseData(list, expected))
+
+[<TestCaseSource("testData")>]
+let MapEvenCountTest (list, expected) =
+    Assert.AreEqual(expected, evenCountMap list)
+
+[<TestCaseSource("testData")>]
+let FilterEvenCountTest (list, expected) = 
+    Assert.AreEqual(expected, evenCountFilter list)
+
+[<TestCaseSource("testData")>]
+let FoldEvenCountTest (list, expected) = 
+    Assert.AreEqual(expected, evenCountFold list)
