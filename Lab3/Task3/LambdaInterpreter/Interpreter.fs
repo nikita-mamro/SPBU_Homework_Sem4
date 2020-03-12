594b9973266bfc1bd522d52f82d6f501871f363f
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2020-03-12 22:19:57 +0300

    Updated alpha conversion and started implementing beta reduction

--- Lab3/Task3/LambdaInterpreter/Interpreter.fs
+++ Lab3/Task3/LambdaInterpreter/Interpreter.fs
@@ -1,35 +1,37 @@
 ﻿module Interpreter
 
+type VariablesSet = char
+
 type LambdaTerm =
-    | Variable of char
+    | Variable of VariablesSet
     | Application of LambdaTerm * LambdaTerm
-    | Abstraction of list<char> * LambdaTerm
-    override this.ToString() =
+    | Abstraction of VariablesSet * LambdaTerm
+
+    override this.ToString() = 
         match this with
-        | Variable name -> 
+        | Variable name ->
             name.ToString()
-        | Application (f, arg) -> 
-            sprintf "(%A %A)" f arg
-        | Abstraction (parameters, body) -> 
-            sprintf ("Л%A.%A") parameters body
+        | Application (f, arg) ->
+            ["("; f.ToString(); " "; arg.ToString(); ")"] |> Seq.fold (+) ""
+        | Abstraction (parameter, body) ->
+            ["Л"; parameter.ToString(); "."; body.ToString()] |> Seq.fold (+) ""
 
 let rec occurs name = function
     | Variable varName -> 
         varName = name
     | Application (f, arg) ->
         occurs name f || occurs name arg
-    | Abstraction (parameters, body) ->
-        List.contains name parameters || occurs name body
+    | Abstraction (parameter, body) ->
+        name = parameter || occurs name body
 
 let rec occursFree name = function
     | Variable varName ->
         varName = name
     | Application (f, arg) ->
         occursFree name f || occursFree name arg
-    | Abstraction (parameters, body) ->
-        not (List.contains name parameters) && occursFree name body
+    | Abstraction (parameter, body) ->
+        name <> parameter && occursFree name body
 
-// no double occurance support    
 let alphaConvert oldName newName term =
 
     let rec convert oldName newName expression =
@@ -42,19 +44,39 @@ let alphaConvert oldName newName term =
         | Application (f, arg) ->
             Application(convert oldName newName f, 
                         convert oldName newName arg)
-        | Abstraction(parameters, body) -> 
-            Abstraction(parameters, convert oldName newName body)
+        | Abstraction(parameter, body) ->
+            if parameter = oldName then
+                Abstraction(newName, convert oldName newName body)
+            else
+                Abstraction(parameter, convert oldName newName body)
 
-    if (occurs newName term) then
-        failwithf "%c already occurs" newName
+    if (occursFree newName term) then
+        failwithf "Error: New name %c occurs free in term" newName
     match term with
-    | Abstraction(parameters, body) ->
-        if not (List.contains oldName parameters) then
-            failwithf "%c does not exist" oldName
-            // need to update list of parameters
-        Abstraction(parameters, convert oldName newName body)
-    | _ -> failwith "Only abstraction can be alpha converted"
+    | Abstraction(parameter, body) ->
+        if oldName <> parameter then
+            failwithf "Error: Old name %c does not exist" oldName
+        Abstraction(newName, convert oldName newName body)
+    | _ -> failwith "Error: Only abstraction can be alpha converted"
         
-        
+let rec substitute arg parameter body = 
+    match body with
+    | Variable name->
+        if name = parameter then arg else body
+    | Application (f, arg')->
+         Application (substitute arg parameter f, substitute arg parameter arg')
+    | Abstraction (parameter', body') -> 
+        if parameter' = parameter then 
+            body
+        elif occursFree parameter' arg then
+            // todo
+            alphaConvert parameter' 'Ы' body'
+        else
+            Abstraction (parameter', substitute arg parameter body')
 
-printfn "%A" <| Abstraction(['x'; 'y'], Application(Variable('x'), Variable('x'))).ToString()
+let betaReduction = function
+    | Application (Abstraction (parameter, body), arg) ->
+        substitute arg parameter body
+    | expression -> failwithf "Error: %A is not a beta redex" expression
+
+printfn "%A" <| (betaReduction (Application(Abstraction('x', Variable('x')), Abstraction('x', Variable('x'))))).ToString()
