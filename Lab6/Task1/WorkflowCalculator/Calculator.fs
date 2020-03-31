c41a07dd3e9a70df3c3fbd5f5f89ccd193c6c942
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2020-03-31 12:15:37 +0300

    'match' style fix

--- Lab6/Task1/WorkflowCalculator/Calculator.fs
+++ Lab6/Task1/WorkflowCalculator/Calculator.fs
@@ -7,7 +7,7 @@ open System
 type CalculationBuilder(accuracy : int) =
     member this.Bind(x, f) =
          match f x with
-            | (res : float) ->
-                Math.Round (res, accuracy)
+         | (res : float) ->
+             Math.Round (res, accuracy)
     member this.Return(x) =
         x
