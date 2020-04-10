0ed80c0518e0d8c9270385525f08d45fb0e0c54c
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2020-04-10 18:50:00 +0300

    Some stack

--- Test/Test/Test/Task3/ThreadSafeStack.fs
+++ Test/Test/Test/Task3/ThreadSafeStack.fs
@@ -1,4 +1,22 @@
 ﻿module ThreadSafeStack
 
+open System
+
 //Реализовать потокобезопасный стек. Стек должен иметь методы Push и TryPop,
 //который возвращает Some <значение в вершине> или None, если стек пуст. Мутабельное состояние использовать можно.
+
+let monitor = new Object()
+
+type Stack<'a> =
+    | Stack of 'a * Stack<'a>
+    | Empty
+
+    member s.Push e =
+        lock monitor (fun() -> Some(Stack(e, s)))
+
+    member s.TryPop =
+        let pop stack =
+            match stack with
+            | Stack(e, stack) -> Some(stack)
+            | Empty -> None
+        lock monitor (fun() -> pop s)
