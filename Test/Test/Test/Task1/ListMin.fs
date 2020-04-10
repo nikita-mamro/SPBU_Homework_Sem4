062d553948139b28bbf2dcd1209bcad9f523a1cb
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2020-04-10 18:20:51 +0300

    Added first 2 tasks

---
+++ Test/Test/Test/Task1/ListMin.fs
@@ -0,0 +1,17 @@
+﻿module ListMin
+
+//Task:
+//Найти наименьший элемент в списке, не используя рекурсию и List.min.
+//Конструкции императивного программирования использовать нельзя.
+
+let min = function
+    | [] -> failwith "Empty list"
+    | head :: tail ->
+        let rec recMin currentMin = function
+            | [] -> currentMin
+            | h :: t ->
+                if h < currentMin then
+                    recMin h t
+                else
+                    recMin currentMin t
+        recMin head tail
