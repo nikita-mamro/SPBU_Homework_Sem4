77557883a6ba1f48d9e02ab194bfd786da01cd4d
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2020-05-15 13:47:07 +0300

    Added some queue code

--- Test/PriorityQueue/PriorityQueue.fs
+++ Test/PriorityQueue/PriorityQueue.fs
@@ -1,2 +1,20 @@
 ﻿module PriorityQueue
 
+/// Priority queue structure
+/// Stores pairs (value, priority)
+type PriorityQueue<'a>() =
+    /// List of stored pairs
+    let mutable list = []
+
+    /// Enqueues item with given priority
+    member this.Enq value priority =
+        ""
+
+    /// Dequeues item with highest priority
+    member this.Deq () =
+        match list with
+        | [] ->
+            invalidOp("Can't dequeue from empty queue.")
+        | h :: t ->
+            list <- t
+            h
