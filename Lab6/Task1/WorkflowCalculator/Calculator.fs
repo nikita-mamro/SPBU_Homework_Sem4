44217659bf29d39334a918b296f86ff2586602da
Nikita Mamro <43335327+zzzloba@users.noreply.github.com>
2020-06-03 11:46:31 +0300

    Fixed workflow

--- Lab6/Task1/WorkflowCalculator/Calculator.fs
+++ Lab6/Task1/WorkflowCalculator/Calculator.fs
@@ -5,9 +5,7 @@ open System
 // Workflow builder for float calculations
 // with a certain accuracy
 type CalculationBuilder(accuracy : int) =
-    member this.Bind(x, f) =
-         match f x with
-         | (res : float) ->
-             Math.Round (res, accuracy)
-    member this.Return(x) =
-        x
+    member this.Bind(x : float, f : float -> float) =
+        Math.Round (f x, accuracy)
+    member this.Return(x : float) =
+        Math.Round (x, accuracy)
